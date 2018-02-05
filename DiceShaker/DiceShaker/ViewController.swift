1fc55d7d42eee72a63c6f1dd92b4d1d2b02eff54
Sean Edward Comerford <seanedward@Seans-MacBook-Pro.local>
2018-02-05 22:37:31 +0000

    Second Commit

---
+++ DiceShaker/DiceShaker/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  DiceShaker
+//
+//  Created by Sean Edward Comerford on 05/02/2018.
+//  Copyright © 2018 Sean Edward Comerford. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var diceImageView1: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
